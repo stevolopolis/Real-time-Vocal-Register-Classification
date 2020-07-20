4620917f896880194baa8ed62f4a0e0b8d951375
stevolopolis <45874944+stevolopolis@users.noreply.github.com>
2020-07-20 13:53:19 +0800

    Completed code for checking stastistics of data

--- Investigation/data_bias.py
+++ Investigation/data_bias.py
@@ -9,22 +9,40 @@ def check_if_num(string):
         return False
 
 
-n_chest = 0
-n_mix = 0
-n_head = 0
-n_falsetto = 0
-n_whistle = 0
+def change_numbering(data_path):
+    # change numbering to 3-digit numbering e.g. 001, 021, 103
+    for file in os.listdir(data_path):
+        if check_if_num(file[-6]):
+            os.rename(os.path.join(data_path, file), os.path.join(data_path, file[:-6] + '0' + file[-6:]))
+        elif check_if_num(file[-7]):
+            pass
+        else:
+            os.rename(os.path.join(data_path, file), os.path.join(data_path, file[:-5] + '00' + file[-5:]))
+ 
 
-data_path = 'D:/AI Project Group/VOCREG/DATA/Labelled_data/STFT_Jul20'
+def get_n_registers(data_path):
+    # Get the respective number of samples per register
+    n_chest = 0
+    n_mix = 0
+    n_head = 0
+    n_falsetto = 0
+    n_whistle = 0
+    n_controversial = 0
 
-# change numbering to 3-digit numbering e.g. 001, 021, 103
-for file in os.listdir(data_path):
-    if check_if_num(file[-6]):
-        os.rename(os.path.join(data_path, file), os.path.join(data_path, file[:-7] + '0' + file[-7:]))
-    elif check_if_num(file[-7]):
-        pass
-    else:
-        print(os.path.join(data_path, file[:-6] + '00' + file[-6:]))
-        input()
-        os.rename(os.path.join(data_path, file), os.path.join(data_path, file[:-6] + '00' + file[-6:]))
+    for file in os.listdir(data_path):
+        str_ls = file.split('_')
+        if str_ls[-2] == 'chest':
+            n_chest += 1
+        elif str_ls[-2] == 'mixed':
+            n_mix += 1
+        elif str_ls[-2] == 'head':
+            n_head += 1
+        elif str_ls[-2] == 'falsetto':
+            n_falsetto += 1
+        elif str_ls[-2] == 'controversial':
+            n_controversial += 1
+        elif str_ls[-2] == 'whistle':
+            n_whistle += 1
+
+    return [n_chest, n_mix, n_head, n_falsetto, n_whistle, n_controversial]
         
