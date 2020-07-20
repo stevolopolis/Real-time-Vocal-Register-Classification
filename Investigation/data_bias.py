7f09a0085afc1435dd63b534d5ec13e3f7c4b229
stevolopolis <45874944+stevolopolis@users.noreply.github.com>
2020-07-20 13:59:34 +0800

    Modified data_bias.py from function to class

--- Investigation/data_bias.py
+++ Investigation/data_bias.py
@@ -9,40 +9,55 @@ def check_if_num(string):
         return False
 
 
-def change_numbering(data_path):
-    # change numbering to 3-digit numbering e.g. 001, 021, 103
-    for file in os.listdir(data_path):
-        if check_if_num(file[-6]):
-            os.rename(os.path.join(data_path, file), os.path.join(data_path, file[:-6] + '0' + file[-6:]))
-        elif check_if_num(file[-7]):
-            pass
-        else:
-            os.rename(os.path.join(data_path, file), os.path.join(data_path, file[:-5] + '00' + file[-5:]))
- 
+class StatisticsCounter():
+    def __init__(self, data_path):
+        self.data_path = data_path
 
-def get_n_registers(data_path):
-    # Get the respective number of samples per register
-    n_chest = 0
-    n_mix = 0
-    n_head = 0
-    n_falsetto = 0
-    n_whistle = 0
-    n_controversial = 0
+    def change_numbering(self):
+        # change numbering to 3-digit numbering e.g. 001, 021, 103
+        for file in os.listdir(self.data_path):
+            if check_if_num(file[-6]):
+                os.rename(os.path.join(self.data_path, file), os.path.join(self.data_path, file[:-6] + '0' + file[-6:]))
+            elif check_if_num(file[-7]):
+                pass
+            else:
+                os.rename(os.path.join(self.data_path, file), os.path.join(self.data_path, file[:-5] + '00' + file[-5:]))
+    
 
-    for file in os.listdir(data_path):
-        str_ls = file.split('_')
-        if str_ls[-2] == 'chest':
-            n_chest += 1
-        elif str_ls[-2] == 'mixed':
-            n_mix += 1
-        elif str_ls[-2] == 'head':
-            n_head += 1
-        elif str_ls[-2] == 'falsetto':
-            n_falsetto += 1
-        elif str_ls[-2] == 'controversial':
-            n_controversial += 1
-        elif str_ls[-2] == 'whistle':
-            n_whistle += 1
+    def get_n_registers(self):
+        # Get the respective number of samples per register
+        self.n_chest = 0
+        self.n_mix = 0
+        self.n_head = 0
+        self.n_falsetto = 0
+        self.n_whistle = 0
+        self.n_controversial = 0
 
-    return [n_chest, n_mix, n_head, n_falsetto, n_whistle, n_controversial]
+        for file in os.listdir(self.data_path):
+            str_ls = file.split('_')
+            if str_ls[-2] == 'chest':
+                self.n_chest += 1
+            elif str_ls[-2] == 'mixed':
+                self.n_mix += 1
+            elif str_ls[-2] == 'head':
+                self.n_head += 1
+            elif str_ls[-2] == 'falsetto':
+                self.n_falsetto += 1
+            elif str_ls[-2] == 'controversial':
+                self.n_controversial += 1
+            elif str_ls[-2] == 'whistle':
+                self.n_whistle += 1
+
+        return [self.n_chest, self.n_mix, self.n_head,
+                self.n_falsetto, self.n_whistle, self.n_controversial]
+
+    def show_n_registers(self):
+        print('Statisitics:\n'
+                'Chest: %s\n'
+                'Mixed: %s\n'
+                'Head: %s\n'
+                'Falsetto: %s\n'
+                'Whistle: %s\n'
+                'Controversial: %s'
+                % (self.n_chest, self.n_mix, self.n_head, self.n_falsetto, self.n_whistle, self.n_controversial))
         
--- Investigation/investigation.py
+++ Investigation/investigation.py
@@ -1,1 +1,7 @@
-from data_bias import get_n_registers
+from data_bias import StatisticsCounter
+
+data_path = 'D:/AI Project Group/VOCREG/DATA/Labelled_data/STFT_Jul20'
+
+Stats = StatisticsCounter(data_path)
+stats_ls = Stats.get_n_registers()
+Stats.show_n_registers()
